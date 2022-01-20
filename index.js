df94e1e0387aa362fe03c03d4c8dc8a9612153e3
Safwa9amar <45048385+Safwa9amar@users.noreply.github.com>
2022-01-20 19:02:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// set date and time
+let time = document.getElementById("time");
+let date = document.getElementById("date");
+// set analog clock
+let seconds = document.querySelector(".seconds");
+let minutes = document.querySelector(".minutes");
+let hours = document.querySelector(".hours");
+
+let timeUpdate = function () {
+  let date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  seconds.style.transform = `rotate(${s * 6}deg)`;
+  minutes.style.transform = `rotate(${m * 6}deg)`;
+  hours.style.transform = `rotate(${h * 30}deg)`;
+  return `${h}:${m}:${s}`;
+};
+
+setInterval(() => {
+  time.textContent = timeUpdate();
+  date.textContent ='Today is : ' + new Date();
+}, 1000);
